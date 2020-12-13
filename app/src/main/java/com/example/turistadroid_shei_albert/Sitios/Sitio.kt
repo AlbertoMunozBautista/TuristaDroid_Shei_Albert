ddb4d9db661d1aaa2b21efe08c48ad585adeb8a5
AlbertoM <albertomunozbautista@gmail.com>
2020-12-13 10:58:49 +0100

    Sitios BBDD
    creación de la base de datos de sitios

--- app/src/main/java/com/example/turistadroid_shei_albert/Sitios/Sitio.kt
+++ app/src/main/java/com/example/turistadroid_shei_albert/Sitios/Sitio.kt
@@ -1,4 +1,44 @@
 package com.example.turistadroid_shei_albert.Sitios
 
-class Sitio {
+import io.realm.RealmObject
+import io.realm.annotations.PrimaryKey
+import java.util.*
+
+//Clase sitio
+open class Sitio (
+    @PrimaryKey var idSitio: String = "",
+    var usuarioLugar: String = "",
+    var nombreLugar: String = "",
+    var lugar: String = "",
+    var fecha: String = "",
+    var estrellas : Int = 0,
+    var latitud: String = "",
+    var longitud: String = "",
+    var comentario: String = "",
+    var foto: String = ""
+
+): RealmObject() {
+    constructor(
+        usuarioLugar: String,
+        nombreLugar: String,
+        lugar: String,
+        fecha: String,
+        estrellas: Int,
+        latitud: String,
+        longitud: String,
+        comentario: String,
+        foto: String
+    ) : this (
+        (UUID.randomUUID().toString()),
+        usuarioLugar,
+        nombreLugar,
+        lugar,
+        fecha,
+        estrellas,
+        latitud,
+        longitud,
+        comentario,
+        foto
+    )
+
 }
